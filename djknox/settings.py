b9f1e15cebe221e784bd0f9277c23261990a7e79
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-08-22 17:27:14 +0300

    build frontend3

--- djknox/settings.py
+++ djknox/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-2xo08qh1jn4+w#h9m&2j@++4kbetw3s(n3!rb!%&o4j4xy1#p*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
