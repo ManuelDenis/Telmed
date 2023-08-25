fed6b1111eda35f745bf29e3ab30bfcf6516f4d7
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-08-25 13:08:51 +0300

    338

--- djknox/settings.py
+++ djknox/settings.py
@@ -84,21 +84,12 @@ WSGI_APPLICATION = 'djknox.wsgi.application'
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
         'ENGINE': os.environ.get('ENGINEh'),
         'NAME': os.environ.get('NAMEh'),
         'USER': os.environ.get('USERh'),
         'PASSWORD': os.environ.get('PASSWORDh'),
         'HOST': os.environ.get('HOSTh'),
         'PORT': os.environ.get('PORTh'),
-=======
-        'ENGINE': os.environ.get('ENGINE'),
-        'NAME': os.environ.get('NAME'),
-        'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('PASSWORD'),
-        'HOST': os.environ.get('HOST'),
-        'PORT': os.environ.get('PORT'),
->>>>>>> a5c35de4ee01a65aa15d2c31487160874e92754c
     }
 }
 
