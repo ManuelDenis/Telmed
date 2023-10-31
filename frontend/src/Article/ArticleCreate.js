91d0249d7415dbeedeb7785dc268f71fc68f58c8
ManuelDenis <popescu.manuel.denis@gmail.com>
2023-10-31 09:06:55 +0200

    art4

--- frontend/src/Article/ArticleCreate.js
+++ frontend/src/Article/ArticleCreate.js
@@ -20,7 +20,7 @@ const ArticleCreate = () => {
           title: '',
           content: '',
         });
-        window.location.href = '/Homepage';
+        window.location.href = '/';
     } catch (error) {
         alert(error);
         console.error('Error:', error.response.data);
